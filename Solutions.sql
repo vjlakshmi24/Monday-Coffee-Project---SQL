ae0aa0c22c74f569f2d480a928cc9823a5683b21
Vijayalakshmi Balaji <vijimimu1995@gmail.com>
2025-02-07 22:55:37 +0000

    Solutions.sql

--- Solutions.sql
+++ Solutions.sql
@@ -1,15 +1,6 @@
 -- Monday Coffee -- Data Analysis 
 
-SELECT * FROM city;
-SELECT * FROM products;
-SELECT * FROM customers;
-SELECT * FROM sales;
-
--- Reports & Data Analysis
-
-
--- Q.1 Coffee Consumers Count
--- How many people in each city are estimated to consume coffee, given that 25% of the population does?
+-- Q.1 How many people in each city are estimated to consume coffee, given that 25% of the population does?
 
 SELECT 
 	city_name,
@@ -20,9 +11,7 @@ SELECT
 FROM city
 ORDER BY 2 DESC
 
--- -- Q.2
--- Total Revenue from Coffee Sales
--- What is the total revenue generated from coffee sales across all cities in the last quarter of 2023?
+-- -- Q.2 What is the total revenue generated from coffee sales across all cities in the last quarter of 2023?
 
 
 SELECT 
@@ -51,9 +40,7 @@ GROUP BY 1
 ORDER BY 2 DESC
 
 
--- Q.3
--- Sales Count for Each Product
--- How many units of each coffee product have been sold?
+-- Q.3 How many units of each coffee product have been sold?
 
 SELECT 
 	p.product_name,
@@ -65,12 +52,8 @@ ON s.product_id = p.product_id
 GROUP BY 1
 ORDER BY 2 DESC
 
--- Q.4
--- Average Sales Amount per City
--- What is the average sales amount per customer in each city?
+-- Q.4 What is the average sales amount per customer in each city?
 
--- city abd total sale
--- no cx in each these city
 
 
 SELECT 
@@ -127,9 +110,7 @@ ON city_table.city_name = customers_table.city_name
 
 
 
--- -- Q6
--- Top Selling Products by City
--- What are the top 3 selling products in each city based on sales volume?
+-- -- Q6 What are the top 3 selling products in each city based on sales volume?
 
 SELECT * 
 FROM -- table
@@ -152,9 +133,7 @@ FROM -- table
 WHERE rank <= 3
 
 
--- Q.7
--- Customer Segmentation by City
--- How many unique customers are there in each city who have purchased coffee products?
+-- Q.7 How many unique customers are there in each city who have purchased coffee products?
 
 SELECT * FROM products;
 
@@ -174,11 +153,7 @@ WHERE
 GROUP BY 1
 
 
--- -- Q.8
--- Average Sale vs Rent
--- Find each city and their average sale per customer and avg rent per customer
-
--- Conclusions
+-- -- Q.8 Find each city and their average sale per customer and avg rent per customer
 
 WITH city_table
 AS
@@ -223,10 +198,7 @@ ORDER BY 4 DESC
 
 
 
--- Q.9
--- Monthly Sales Growth
--- Sales growth rate: Calculate the percentage growth (or decline) in sales over different time periods (monthly)
--- by each city
+-- Q.9 Sales growth rate: Calculate the percentage growth (or decline) in sales over different time periods (monthly) by each city
 
 WITH
 monthly_sales
@@ -273,9 +245,7 @@ WHERE
 	last_month_sale IS NOT NULL	
 
 
--- Q.10
--- Market Potential Analysis
--- Identify top 3 city based on highest sales, return city name, total sale, total rent, total customers, estimated coffee consumer
+-- Q.10 Identify top 3 city based on highest sales, return city name, total sale, total rent, total customers, estimated coffee consumer
 
 
 
@@ -325,21 +295,6 @@ ON cr.city_name = ct.city_name
 ORDER BY 2 DESC
 
 /*
--- Recomendation
-City 1: Pune
-	1.Average rent per customer is very low.
-	2.Highest total revenue.
-	3.Average sales per customer is also high.
-
-City 2: Delhi
-	1.Highest estimated coffee consumers at 7.7 million.
-	2.Highest total number of customers, which is 68.
-	3.Average rent per customer is 330 (still under 500).
-
-City 3: Jaipur
-	1.Highest number of customers, which is 69.
-	2.Average rent per customer is very low at 156.
-	3.Average sales per customer is better at 11.6k.
 
 
 
